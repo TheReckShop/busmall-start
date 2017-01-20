10ea8c15f7bd002d6693859756c72d0817604829
thereckshop <thereckshop@mac.com>
2017-01-20 09:06:53 -0800

    got click amount to work

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ var pic3 = document.createElement('pic-3');
 var productName = ['bag', 'bananna', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 var imageFilePath = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
 
+var leftProduct;
+var rightProduct;
+var centerProduct;
 function ImageConstructor(imageName, imageFilePath, timesShown, timesClicked) {
   this.imageName = imageName;
   this.imageFilePath = imageFilePath;
-  this.timesShown = timesShown;
-  this.timesClicked = timesClicked;
+  this.timesShown = 0;
+  this.timesClicked = 0;
 };
 
 var bag = new ImageConstructor(productName[0], imageFilePath[0], 0, 0);
@@ -37,13 +40,19 @@ var wineGlass = new ImageConstructor(productName[19], imageFilePath[19], 0, 0);
 
 var choicesMade = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
 
+
+// parseInt(totalClicks);
+
 function randNum() {
   var min = 0;
   var max = 19;
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-function uploadImg() {
+function uploadImgLeft() {
+  var totalClicks = bag.timesClicked + banana.timesClicked + bathroom.timesClicked + boots.timesClicked + breakfast.timesClicked + bubblegum.timesClicked + chair.timesClicked + cthulhu.timesClicked + dogDuck.timesClicked + dragon.timesClicked + pen.timesClicked + petSweep.timesClicked + scissors.timesClicked + shark.timesClicked + sweep.timesClicked + tauntaun.timesClicked + unicorn.timesClicked + usb.timesClicked + waterCan.timesClicked + wineGlass.timesClicked;
+  console.log('TC: ' + totalClicks);
+  console.log('TC: ' + totalClicks);
   var leftIndex = randNum();
   console.log('start');
   var leftProduct = choicesMade[leftIndex];
@@ -51,6 +60,57 @@ function uploadImg() {
   left.alt = leftProduct.productName;
   leftProduct.timesShown += 1;
   leftProduct.timesClicked += 1;
+  var centerIndex = randNum();
+  while (centerIndex === leftIndex) {
+    centerIndex = randNum();
+  }
+
+  var centerProduct = choicesMade[centerIndex];
+  center.src = centerProduct.imageFilePath;
+  center.alt = centerProduct.productName;
+  // centerProduct.timesShown += 1;
+  // centerProduct.timesClicked += 1;
+  var rightIndex = randNum();
+  while (rightIndex === leftIndex || rightIndex === centerIndex) {
+    rightIndex = randNum();
+  }
+
+  var rightProduct = choicesMade[rightIndex];
+  right.src = rightProduct.imageFilePath;
+  right.alt = rightProduct.productName;
+  // rightProduct.timesShown += 1;
+  // rightProduct.timesClicked += 1;
+
+  var previouslyShown = [leftIndex, rightIndex, centerIndex];
+  console.log('PS' + previouslyShown);
+  while (previouslyShown[0] === previouslyShown[1] || previouslyShown[0] === previouslyShown[2]) {
+    leftIndex = randNum();
+    console.log('test1');
+    break;
+  }
+  while (previouslyShown[1] === previouslyShown[2]) {
+    rightIndex = randNum();
+    console.log('test2');
+    break;
+  }
+  console.log('timesClicked ' + leftProduct.timesClicked + rightProduct.timesClicked + centerProduct.timesClicked);
+
+  while (leftProduct.timesClicked + rightProduct.timesClicked + centerProduct.timesClicked === 5) {
+    console.log('test');
+    break;
+    // var text = document.getElementById('totals').textContent;
+    // document.getElementById('totals').textContent = 'This is some text'
+  }
+};
+
+function uploadImgCenter() {
+  var leftIndex = randNum();
+  console.log('start');
+  var leftProduct = choicesMade[leftIndex];
+  left.src = leftProduct.imageFilePath;
+  left.alt = leftProduct.productName;
+  // leftProduct.timesShown += 1;
+  // leftProduct.timesClicked += 1;
   var centerIndex = randNum();
   while (centerIndex === leftIndex) {
     centerIndex = randNum();
@@ -69,8 +129,8 @@ function uploadImg() {
   var rightProduct = choicesMade[rightIndex];
   right.src = rightProduct.imageFilePath;
   right.alt = rightProduct.productName;
-  rightProduct.timesShown += 1;
-  rightProduct.timesClicked += 1;
+  // rightProduct.timesShown += 1;
+  // rightProduct.timesClicked += 1;
 
   var previouslyShown = [rightIndex, leftIndex, centerIndex];
   console.log(previouslyShown);
@@ -82,18 +142,68 @@ function uploadImg() {
     rightIndex = randNum();
     break;
   }
-  // console.log(leftProduct.timesClicked + rightProduct.timesClicked + centerProduct.timesClicked);
+  console.log('timesClicked ' + leftProduct.timesClicked + rightProduct.timesClicked + centerProduct.timesClicked);
 
-  while (leftProduct + rightProduct + centerProduct === 25) {
+  while (leftProduct.timesClicked + rightProduct.timesClicked + centerProduct.timesClicked === 5) {
+    console.log('test');
+  };
+};
+function uploadImgRight() {
+  var leftIndex = randNum();
+  console.log('start');
+  var leftProduct = choicesMade[leftIndex];
+  left.src = leftProduct.imageFilePath;
+  left.alt = leftProduct.productName;
+  // leftProduct.timesShown += 1;
+  // leftProduct.timesClicked += 1;
+  var centerIndex = randNum();
+  while (centerIndex === leftIndex) {
+    centerIndex = randNum();
+  }
+
+  var centerProduct = choicesMade[centerIndex];
+  center.src = centerProduct.imageFilePath;
+  center.alt = centerProduct.productName;
+  // centerProduct.timesShown += 1;
+  // centerProduct.timesClicked += 1;
+  var rightIndex = randNum();
+  while (rightIndex === leftIndex || rightIndex === centerIndex) {
+    rightIndex = randNum();
+  }
+
+  var rightProduct = choicesMade[rightIndex];
+  right.src = rightProduct.imageFilePath;
+  right.alt = rightProduct.productName;
+  rightProduct.timesShown += 1;
+  rightProduct.timesClicked += 1;
+
+  var previouslyShown = [rightIndex, leftIndex, centerIndex];
+  console.log(previouslyShown);
+  while (previouslyShown[0] === previouslyShown[1] || previouslyShown[0] === previouslyShown[2]) {
+    rightIndex = randNum();
+    previouslyShown[0] = rightIndex;
+    console.log(' rightIndex' + rightIndex );
+  }
+  while (previouslyShown[1] === previouslyShown[2]) {
+    leftIndex = randNum();
+    previouslyShown[1] = leftIndex;
+    console.log('leftindex' + leftIndex);
+  }
+  console.log('timesClicked ' + leftProduct.timesClicked + rightProduct.timesClicked + centerProduct.timesClicked);
+
+  while (leftProduct.timesClicked + rightProduct.timesClicked + centerProduct.timesClicked === 5) {
+    console.log('test');
     // var text = document.getElementById('totals').textContent;
     // document.getElementById('totals').textContent = 'This is some text'
     break;
   }
 };
-uploadImg();
+uploadImgLeft();
+uploadImgRight();
+uploadImgCenter();
 var imgElLeft = document.getElementById('left');
 var imgElCenter = document.getElementById('center');
 var imgElRight = document.getElementById('right');
-imgElLeft.addEventListener('click', uploadImg, false);
-imgElRight.addEventListener('click', uploadImg, false);
-imgElCenter.addEventListener('click', uploadImg, false)
+imgElLeft.addEventListener('click', uploadImgLeft, false);
+imgElRight.addEventListener('click', uploadImgRight, false);
+imgElCenter.addEventListener('click', uploadImgCenter, false);
